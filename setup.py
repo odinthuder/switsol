119811a9945248e9c73dfad010aeec7b423db549
odinthuder <zakaria.mak.study@gmail.com>
2022-05-03 22:06:03 +0000

    Some Changes to setup.py updated the code  - Changes to install_requires  - Change to import

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements # changed pip.req to pip._internal.req
 import re, ast
+
 
 # get version from __version__ variable in switsol/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -10,7 +11,7 @@ with open('switsol/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+requirements = parse_requirements("requirements.txt", session="false")	
 
 setup(
 	name='switsol',
@@ -21,6 +22,11 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
+	install_requires=[str(ir.requirement) for ir in requirements], # changed to str(ir.req) to str(ir.requirement)
 	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
+
+
+
+#!/usr/bin/env python3
+# Path: setup.py
